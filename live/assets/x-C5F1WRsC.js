9ff17521aa5b860632eb9d51d883b0913c773dd1
aymred <aymred@gmail.com>
2026-08-11 16:16:11 -0500

    no hay changes

--- live/assets/x-C5F1WRsC.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./BrandLogo-CYh7tZiU.js";var t=e(`message-circle`,[[`path`,{d:`M2.992 16.342a2 2 0 0 1 .094 1.167l-1.065 3.29a1 1 0 0 0 1.236 1.168l3.413-.998a2 2 0 0 1 1.099.092 10 10 0 1 0-4.777-4.719`,key:`1sd12s`}]]),n=e(`x`,[[`path`,{d:`M18 6 6 18`,key:`1bl5f8`}],[`path`,{d:`m6 6 12 12`,key:`d8bk6v`}]]);export{t as n,n as t};
